ba1594d6d3481d15179cfa2e79c586e1d072b717
Jaco Retief <jaco.retief@gmail.com>
2026-04-01 20:34:48 -0600

    fix: use new ImagePicker mediaTypes array syntax

--- app/profile/personalise.tsx
+++ app/profile/personalise.tsx
@@ -37,7 +37,7 @@ export default function PersonaliseScreen() {
       return;
     }
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ['images'],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.8,
